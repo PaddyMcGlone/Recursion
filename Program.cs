fff7f39da567f8d4d54b55984cf7daf571d0a734
Paddy McGlone <paddymcglone@gmail.com>
2019-09-02 07:44:38 +0100

    Update Program.cs
    - Adding a simple recursive example.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,30 @@ namespace Recursion
 {
     class Program
     {
+        // My first recursive program
+        static int CalculateRecursively(int n, int m)
+        {            
+            if(n < m)
+            {
+                n++;
+                CalculateRecursively(n, m);
+            }
+
+            return n;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter number N:");
+            var N = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number M:");
+            var M = Convert.ToInt32(Console.ReadLine());
+
+            int sum = CalculateRecursively(N, M);
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.ReadLine();
         }
     }
 }
